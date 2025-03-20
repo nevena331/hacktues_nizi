166a006535f2007ee1f17b87b8c2f15946c681c0
Dimitar Binev <dimitar.b.binev.2023@elsys-bg.org>
2025-03-20 18:23:23 +0200

    Full commit for new linkings and requirements

--- SmartSpend/SmartSpend/settings.py
+++ SmartSpend/SmartSpend/settings.py
@@ -46,7 +46,7 @@ ROOT_URLCONF = 'SmartSpend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [BASE_DIR, "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -112,11 +112,14 @@ AUTH_USER_MODEL = 'finance_track.User'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / '/static/']
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG') == 'True'
+
+
+REVOLUT_API_KEY = os.getenv("REVOLUT_API_KEY")
