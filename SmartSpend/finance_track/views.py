9355060d9492f6a2d1157392bd916c9061898380
Dimitar Binev <dimitar.b.binev.2023@elsys-bg.org>
2025-03-20 14:31:03 +0200

    First test url and view

--- SmartSpend/finance_track/views.py
+++ SmartSpend/finance_track/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("Hello")
