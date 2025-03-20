9574f5872dff8286e112fa33b1056e84e36a856d
Georgi Pastrakov <160168282+GeorgiMPastrakov@users.noreply.github.com>
2025-03-21 01:56:12 +0200

    Other changes

--- SmartSpend/finance_track/urls.py
+++ SmartSpend/finance_track/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.frontpage),
     path('test/', views.test),
     path('connect_truelayer/', views.connect_truelayer, name='connect_truelayer'),
+    path('truelayer/callback/', views.truelayer_callback, name='truelayer_callback'),
 ]
