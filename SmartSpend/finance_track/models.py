9ec598f5f1987efe536e3b731b54f3de327b5725
Georgi Pastrakov <160168282+GeorgiMPastrakov@users.noreply.github.com>
2025-03-20 22:18:14 +0200

    Remove duplicate imports in models.py

--- SmartSpend/finance_track/models.py
+++ SmartSpend/finance_track/models.py
@@ -1,8 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-from django.contrib.auth.models import AbstractUser
-from django.db import models
 
 class User(AbstractUser):
     revolut_id = models.CharField(max_length=255, blank=True, null=True)
