52f7bbd50cf1649b264adc79e7976b333dfc40ff
Dimitar Binev <dimitar.b.binev.2023@elsys-bg.org>
2025-03-21 15:06:09 +0200

    Add registration form with password validation

---
+++ SmartSpend/finance_track/forms.py
@@ -0,0 +1,20 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class RegistrationForm(forms.ModelForm):
+    password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
+    password2 = forms.CharField(label='Confirm Password', widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['email', 'first_name', 'last_name']
+
+    def clean(self):
+        cleaned_data = super().clean()
+        p1 = cleaned_data.get('password1')
+        p2 = cleaned_data.get('password2')
+        if p1 and p2 and p1 != p2:
+            self.add_error('password2', 'Passwords do not match')
+        return cleaned_data
