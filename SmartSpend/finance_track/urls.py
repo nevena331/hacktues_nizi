2497bedab60fafc36d59e80b1bc1e56657378611
Georgi Pastrakov <160168282+GeorgiMPastrakov@users.noreply.github.com>
2025-03-21 23:22:37 +0200

    removed auth

--- SmartSpend/finance_track/urls.py
+++ SmartSpend/finance_track/urls.py
@@ -10,7 +10,6 @@ urlpatterns = [
     path('receipt_detail/', views.receipt_detail),
     path('receipt_results/', views.receipt_results),
     path('add_expense/', views.add_expense),
-    path('auth/', views.auth),
     path('connect_truelayer/', views.connect_truelayer, name='connect_truelayer'),
     path('truelayer/callback/', views.truelayer_callback, name='truelayer_callback'),
     path("receipt/<int:receipt_id>/process/", process_receipt, name="process_receipt"),
