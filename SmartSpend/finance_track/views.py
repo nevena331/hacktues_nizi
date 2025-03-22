667ee6a0a7e431f07a1cf71c6419c27d3c0005e2
Georgi Pastrakov <160168282+GeorgiMPastrakov@users.noreply.github.com>
2025-03-22 02:13:44 +0200

    Remove username parameter from user creation in register_view

--- SmartSpend/finance_track/views.py
+++ SmartSpend/finance_track/views.py
@@ -146,7 +146,7 @@ def register_view(request):
         
         # Create the new user.
         # Here, we use the email as the username.
-        user = User.objects.create_user(username=email, email=email, password=password)
+        user = User.objects.create_user(email=email, password=password)
         user.first_name = first_name
         user.last_name = last_name
         user.save()
