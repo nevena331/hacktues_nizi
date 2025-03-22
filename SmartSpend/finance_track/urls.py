eff20aa9559f2ef66f93e9c36b781e223df3f30a
Dimitar Binev <dimitar.b.binev.2023@elsys-bg.org>
2025-03-22 03:25:35 +0200

    Update login and logout paths in urls.py to include next_page redirection

--- SmartSpend/finance_track/urls.py
+++ SmartSpend/finance_track/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('truelayer/callback/', views.truelayer_callback, name='truelayer_callback'),
     path("receipt/<int:receipt_id>/process/", process_receipt, name="process_receipt"),
     path('register/', views.register_view, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='finance_track/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='finance_track/dashboard.html'), name='logout'),
+    path('login/', auth_views.LoginView.as_view(template_name='finance_track/login.html'), name='login', next_page='finance_track/dashboard.html'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='finance_track/homepage.html'), name='logout'),
 ]
